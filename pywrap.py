65217c0bf4286ec9b31c29795f1828fc1836134d
Armin <ap@ul30.linux-local>
2020-10-04 02:53:00 +0200

    suche nach MYPYLIB eingefügt

--- pywrap.py
+++ pywrap.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """ Hilfsprogramm zur Suche und zum Start von Python Programmen. 
+    Es ist notwendig, dass der private Bibliothekspad über die
+    Environment Variable MYPYLIB gesetzt ist, andernfalls wird im 
+    Homeverzeichnis des Users nach einem Directory mit dem Namen 
+    pylibname (zu setzen unter diesem doctext) gesucht. 
 """
+pylibname = 'pylibap'
 ##############################################################################
-import os, os.path, sys
+import os, os.path, sys, re
 (prgdir,fname) = os.path.split(os.path.abspath(__file__))
 (prgname,ext) = os.path.splitext(fname)
 globals()['prgname'] = prgname
@@ -29,6 +34,32 @@ if py:
     args.insert( 0, sys.argv[0] )
     os.execv( py, args )
 
+### ----------------------------------------------------
+def search_dirs(top, searches, ignore = re.compile('^$') ):
+  """ Search for dirs matching one of searches, but
+      ignore everything from ignore
+  """     
+  try:
+    for entry in os.scandir(top):
+      if entry.is_dir():
+#         print(entry.path)
+        if entry.name in searches:
+#          print("-------------- found",entry.name)   
+          yield entry.path
+        elif not ignore.match(entry.name) :
+          for res in search_dirs(entry.path, searches, ignore): 
+            yield res
+  except OSError as e:
+    print(e, file=sys.stderr)
+    pass
+
+### ----------------------------------------------------
+regs = ['^\.','__pycache__', ]
+excludes=re.compile('|'.join(regs))
+#for found in search_dirs(path,[search],excludes):
+#  print(found) 
+#  break
+
 ##############################################################################
 def main():
 ##### Work on sys.argv 
@@ -43,7 +74,7 @@ def main():
   dbg.dprint(2,"Wanted is",prgname, "called from", __file__ , "with args", prgargs )
 ##### Now search for program in Srcdir wait for execution and exit
   # dbg.dprint(1, "Python Version:",sys.version)
-  for top, dirs, files in os.walk(srcdir):
+  for top, dirs, files in os.walk(os.path.dirname(libdir)):
     for nm in files:
       path = os.path.join(top, nm)
       dbg.dprint(4,path)
@@ -59,6 +90,7 @@ def main():
         p.wait()
         dbg.leavesub()
         return
+        
 ##### Nothing found      
   print(prgname, "could not be found")
   dbg.leavesub()
@@ -67,11 +99,12 @@ if __name__ == "__main__":
   import sys
   import os
   try:
-    srcdir = os.environ['MYPYSRC']
     libdir = os.environ['MYPYLIB']
   except KeyError:
-    srcdir = os.environ['MYPYSRC'] = os.path.join(os.environ["HOME"],'pysrc')
-    libdir = os.environ['MYPYLIB'] = os.path.join(os.environ["HOME"],'pysrc/LIBS')
+    for found in search_dirs(path,[pylibname],excludes):
+      print(found) 
+      libdir = found
+      break
   
   try:
     files  = [ os.path.join(libdir, "globaldefs.py"), 
